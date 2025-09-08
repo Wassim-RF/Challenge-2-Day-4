4a5bbc3dcac9a8eafd79693a99a55ce6f2888a43
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 15:35:59 +0200

    #3

--- chall3.c
+++ chall3.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+int compareson(int a , int b) {
+    printf("Entrer le 1er nombre : ");
+    scanf("%d" , &a);
+    printf("Entrer le 2eme nombre : ");
+    scanf("%d" , &b);
+    if (a > b) {
+        printf("%d > %d" , a , b);
+    }else if (a < b) {
+        printf("%d < %d" , a , b);
+    } else {
+        printf("%d = %d" , a , b);
+    }
+    return 1;
+}
+
 int main() {
+    int num1 , num2;
+    compareson(num1 , num2);
     return 0;
 }
