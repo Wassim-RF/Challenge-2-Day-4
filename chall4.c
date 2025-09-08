a1963bd4aa02a2630f2a981a653f2e0ed9ff28f9
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 15:40:18 +0200

    #4

--- chall4.c
+++ chall4.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int minimanx(int a , int b) {
+    printf("Entrer le 1er nombre : ");
+    scanf("%d" , &a);
+    printf("Entrer le 2eme nombre : ");
+    scanf("%d" , &b);
+    if (a < b) {
+        printf("%d < %d" , a , b);
+    }else if (b < a) {
+        printf("%d < %d" , b , a);
+    }
+    return 1;
+}
+
 int main() {
+    int num1 , num2;
+    minimanx(num1 , num2);
     return 0;
 }
