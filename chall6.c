a5059210597003794ca59aa2be4d8fa25cc2b29f
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 16:45:14 +0200

    #6

--- chall6.c
+++ chall6.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+long long int febochine(int num) {
+    int s , i ;
+    int a = 0; 
+    int b = 1;
+    printf("Entrez le nombre de termes : ");
+    scanf("%d", &num);
+    for (i = 0 ; i < num ; i++) {
+        s = a + b;
+        a = b;
+        b = s;
+        printf("%d  " , s);
+    }
+    return 1;
+}
+
 int main() {
+    int num;
+    febochine(num);
     return 0;
 }
