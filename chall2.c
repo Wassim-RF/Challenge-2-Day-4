eca7ae893b3ae050fffbd260668920753df5d930
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 15:31:07 +0200

    #2

--- chall2.c
+++ chall2.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int produit(int a , int b) {
+    printf("Entrer le 1er nombre : ");
+    scanf("%d" , &a);
+    printf("Entrer le 2eme nombre : ");
+    scanf("%d" , &b);
+
+    printf("%d x %d = %d" , a , b , a * b);
+    return 1;
+}
+
 int main() {
+    int num1 , num2;
+    produit(num1 , num2);
     return 0;
 }
