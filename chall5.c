1ccb02d0f0647ada270d5cfa9d5a1aa9eb8b6926
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 16:11:37 +0200

    #5

--- chall5.c
+++ chall5.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int factorial(int num) {
+    int i ;
+    int fact = 1 ;
+    printf("Enter a number : ");
+    scanf("%d" , &num);
+    for (i = 1 ; i <= num - 1 ; i++) {
+        fact *= i ;
+    }
+    long long int factorielle = num * fact;
+    printf("The factorial of your number is %lld\n " , factorial);
+    return factorielle;
+}
+
 int main() {
+    int num;
+    factorial(num);
     return 0;
 }
