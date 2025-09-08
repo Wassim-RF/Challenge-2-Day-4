0ec429ed23c8033df2dd05f37dc7f9a251b74ede
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-08 17:04:39 +0200

    #7

--- chall7.c
+++ chall7.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
+void inverser_chaine(char str[]) {
+    int debut = 0;
+    int fin = strlen(str) - 1;
+    char t;
+    while (debut < fin) {
+        t = str[debut];
+        str[debut] = str[fin];
+        str[fin] = t;
+        debut++;
+        fin--;
+    }
+}
 
 int main() {
+    char mot[32];
+    printf("Entrer un mot : ");
+    scanf("%s" , mot);
+    inverser_chaine(mot);
+    printf("Apres inversion : %s .\n", mot);
     return 0;
 }
